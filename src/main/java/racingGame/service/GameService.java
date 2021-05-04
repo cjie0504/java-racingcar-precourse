788b7948784fca9cb1125347161246e5d7664929
ejhwang <ejhwang@mygenomebox.com>
2021-05-04 11:25:19 +0900

    feat: 어플리케이션 및 GameService 생성

---
+++ src/main/java/racingGame/service/GameService.java
@@ -0,0 +1,12 @@
+package racingGame.service;
+
+import racingGame.domain.UserInput;
+
+public class GameService {
+
+    public static void start(){
+        UserInput userInput = new UserInput();
+        userInput.userInputInfo();
+        new Racing(userInput.carNameList(), userInput.getUserInput_roundCnt());
+    }
+}
