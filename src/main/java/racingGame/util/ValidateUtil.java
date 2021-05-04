2c8be16cfe84c21dcc1fbe8b40d41483d48b995c
ejhwang <ejhwang@mygenomebox.com>
2021-05-04 13:41:29 +0900

    fix: 유효성 검사 Util 파일 수정

--- src/main/java/racingGame/util/ValidateUtil.java
+++ src/main/java/racingGame/util/ValidateUtil.java
@@ -12,23 +12,23 @@ public class ValidateUtil {
 
 
     public static void chkCarName(String carName) {
-        chkCarNameSize(carName);
         chkCarNameEmpty(carName);
+        chkCarNameSize(carName);
     }
 
     public static void chkCarNameEmpty(String carName) {
-        if(carName.length()<1){
+        if (carName.length() < 1) {
             throw new InputException(INPUT_CHK_CAR_NAME_EMPTY);
         }
     }
 
     public static void chkCarNameSize(String carName) {
-        if(carName.length()>5){
+        if (carName.length() > 5) {
             throw new InputException(INPUT_CHK_CAR_NAME_DOWN_5);
         }
     }
 
-    public static void chkRacingRound(String roundCnt){
+    public static void chkRacingRound(String roundCnt) {
         Matcher matcher = RegexUtil.pattern.matcher(roundCnt);
         if (!matcher.find()) {
             throw new InputException(INPUT_CHK_RACING_ROUND_ONLY_NUM);
