bac330aec7841bc449c8b572606bcb28285807bf
ejhwang <ejhwang@mygenomebox.com>
2021-05-04 11:16:35 +0900

    test: 레이싱 검증 테스트 생성

---
+++ src/test/java/racingGame/domain/RacingTest.java
@@ -0,0 +1,21 @@
+package racingGame.domain;
+
+import org.junit.jupiter.api.Test;
+import racingGame.service.Racing;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+public class RacingTest { @Test
+    public void 레이싱_결과_검증(){
+        List<Car> cars = Arrays.asList(
+                new Car("CAR1",5),
+                new Car("CAR2",2),
+                new Car("CAR3",1)
+        );
+        List<String> winCar = Racing.findWinCar(cars,5);
+        assertThat(winCar.get(0)).isEqualTo("CAR1");
+    }
+}
