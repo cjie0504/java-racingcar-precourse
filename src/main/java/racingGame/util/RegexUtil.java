88f44a788448deb1d47fabb2566b61c4cce2368c
ejhwang <ejhwang@mygenomebox.com>
2021-05-04 16:05:54 +0900

    fix: 정규식 수정

--- src/main/java/racingGame/util/RegexUtil.java
+++ src/main/java/racingGame/util/RegexUtil.java
@@ -3,5 +3,5 @@ package racingGame.util;
 import java.util.regex.Pattern;
 
 public class RegexUtil {
-    public static final Pattern pattern = Pattern.compile("(^[1-9]$)");
+    public static final Pattern pattern = Pattern.compile("(^[0-9]*$)");
 }
