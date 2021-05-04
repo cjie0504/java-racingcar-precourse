980d7030f5a4d69afff51099232ee1367e7c392b
ejhwang <ejhwang@mygenomebox.com>
2021-05-04 11:31:07 +0900

    feat: 사용자 입력값으로 cars 생성

---
+++ src/main/java/racingGame/domain/UserInput.java
@@ -0,0 +1,39 @@
+package racingGame.domain;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class UserInput {
+    private static final String INPUT_CAR_NAMES_MSG = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분).";
+    private static final String INPUT_ROUND_MSG = "시도할 회수는 몇회인가요?";
+    private static final String INPUT_CAR_SPLIT_STR = ",";
+
+    private String carNames;
+    private int roundCnt;
+
+    public void userInputInfo(){
+        Scanner scanner = new Scanner(System.in);
+        inputCarName(scanner);
+        inputRoundCnt(scanner);
+    }
+
+    private void inputRoundCnt(Scanner scanner) {
+        System.out.println(INPUT_ROUND_MSG);
+        this.roundCnt = scanner.nextInt();
+    }
+
+    private void inputCarName(Scanner scanner) {
+        System.out.println(INPUT_CAR_NAMES_MSG);
+        this.carNames = scanner.nextLine();
+    }
+
+    public int getUserInput_roundCnt(){
+        return roundCnt;
+    }
+
+    public List<String> carNameList(){
+        List<String> carNameList = Arrays.asList(carNames.split(INPUT_CAR_SPLIT_STR));
+        return carNameList;
+    }
+}
